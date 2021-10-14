247b3a5387da42c1c3ab6262550316743ddf44ed
Gavin Corkery <GavinCorkery@hotmail.com>
2021-10-14 10:43:04 -0400

    Made changes to reply table fk and added video to comments

--- YouTubeCloneAPI/youtubecloneapi/commentsection/models.py
+++ YouTubeCloneAPI/youtubecloneapi/commentsection/models.py
@@ -5,7 +5,8 @@ class Comments(models.Model):
     message =  models.CharField(max_length=280, blank = True, null = True, default = '')
     likes = models.IntegerField(default = 0, null = True, blank = True)
     dislikes = models.IntegerField(default = 0, null = True, blank = True)
+    video = models.CharField(max_length=280, null = True)
 
 class Replies(models.Model):
-    comment_id = models.ForeignKey('commentsection.Comments', on_delete=models.CASCADE, default = '')
+    comment = models.ForeignKey(Comments, on_delete=models.CASCADE, default = '')
     message = models.CharField(max_length=280, blank = True, null = True, default = '')
