5fa5d1bc6368205aa022b6b122bb8bdcaca31c05
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-15 14:49:11 -0400

    Added comments.

--- YouTubeCloneAPI/youtubecloneapi/commentsection/urls.py
+++ YouTubeCloneAPI/youtubecloneapi/commentsection/urls.py
@@ -2,11 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Get all comments by video and used to post
     path('comments/<str:video>/', views.CommentList.as_view()),
+    # Get single comment by comment ID
     path('comment/<int:pk>/', views.CommentDetail.as_view()),
+    # Get all replies and used to post
     path('replies/', views.ReplyList.as_view()),
+    # Get single reply by reply ID
     path('comment/<int:pk>/reply', views.ReplyDetail.as_view()),
+    # Like/Dislike Comment
     path('comment/<int:pk>/thumbs_up', views.CommentLikes.as_view()),
     path('comment/<int:pk>/thumbs_down', views.CommentLikes.as_view()),
+    # Post reply to comment by ID
     path('comment/reply/<int:comment>', views.ReplyList.as_view()),
 ]
