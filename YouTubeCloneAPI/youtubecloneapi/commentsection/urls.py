b13fc8cdf8be05630d48550f7cc7d8fc563bd075
Gavin Corkery <GavinCorkery@hotmail.com>
2021-10-18 10:47:46 -0400

    Fixed url to get replies by comment ID

--- YouTubeCloneAPI/youtubecloneapi/commentsection/urls.py
+++ YouTubeCloneAPI/youtubecloneapi/commentsection/urls.py
@@ -7,12 +7,12 @@ urlpatterns = [
     # Get single comment by comment ID
     path('comment/<int:pk>/', views.CommentDetail.as_view()),
     # Get all replies and used to post
-    path('replies/', views.ReplyList.as_view()),
+    path('replies/<int:comment>', views.ReplyList.as_view()),
     # Get single reply by reply ID
     path('comment/<int:pk>/reply', views.ReplyDetail.as_view()),
     # Like/Dislike Comment
     path('comments/<int:pk>/thumbs_up', views.CommentLikes.as_view()),
     path('comments/<int:pk>/thumbs_down', views.CommentLikes.as_view()),
     # Post reply to comment by ID
-    path('comment/reply/<int:comment>', views.ReplyList.as_view()),
+    path('comments/reply/<int:comment>', views.ReplyList.as_view()),
 ]
